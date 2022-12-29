2703a00388a824370f0547c4f57519d7024223c8
Emine Ayşe Sunar <emineayse1313@gmail.com>
2022-12-30 01:11:40 +0300

    Authentication kismina bir tek scope ekledim, bunun disinda aslinda ayni. Bir de Irem'in bilgileri commente alip kendiminkileri ekledim.

--- spotify.py
+++ spotify.py
@@ -4,13 +4,27 @@ import learnSongs
 
 
 def run():
+    '''Irem:
     client_id = "2327937f76274679975195787e578210"
     client_secret = "fa5d7fc7c5dd42d3b1467df13cf9e2cb"
-    redirect_url = "http://localhost:7777/callback"
+    redirect_url = "http://localhost:7777/callback"'''
 
-    oauth_object = spotipy.SpotifyOAuth(client_id, client_secret, redirect_url)
+    SPOTIFY_CLIENT_ID = "c58413b1659445f1be818dab581cd044"
+    SPOTIFY_CLIENT_SECRET = "5fca9042f09d4f57a3a8e84f2e3a124a"
+    SPOTIFY_REDIRECT_URI = "https://google.com"
+    scope = 'user-library-read user-library-modify playlist-modify-public'
+
+    oauth_object = spotipy.SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
+                                        client_secret=SPOTIFY_CLIENT_SECRET,
+                                        redirect_uri=SPOTIFY_REDIRECT_URI,
+                                        scope=scope)
+
+    token_dict = oauth_object.get_access_token()
+    token = token_dict["access_token"]
+
+    '''oauth_object = spotipy.SpotifyOAuth(client_id, client_secret, redirect_url)
     token_dict = oauth_object.get_cached_token()
-    token = token_dict['access_token']
+    token = token_dict['access_token']'''
 
     if token:
         sp = spotipy.Spotify(auth=token)
