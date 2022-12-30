1af05fd1c114db75751213647c32cff57aeb558e
drnege <37402435+drnege@users.noreply.github.com>
2022-12-30 21:57:58 +0300

    Update open_playlist.py

--- open_playlist.py
+++ open_playlist.py
@@ -1,6 +1,7 @@
 import json
 import spotipy
 import webbrowser
+
 username = 'missframbuaz'
 clientID = '322a5c047ccb4515b49404ed0d8f6065'
 clientSecret = '7818fc966d344e6c82b36c7442f710ee'
@@ -11,18 +12,8 @@ token = token_dict['access_token']
 spotifyObject = spotipy.Spotify(auth=token)
 user = spotifyObject.current_user()
 print(json.dumps(user,sort_keys=True, indent=4))
-while True:
-    print("Welcome, "+ user['display_name'])
-    print("0 - Exit")
-    print("1 - Search for a Playlist")
-    choice = int(input("Your Choice: "))
-    if choice == 1:
-        searchQuery = input("Enter Playlist Id: ")
-        #example id=0mh2I0USWjpJkwe95XdqCU
-        playlist = spotifyObject.playlist(searchQuery)
-        webbrowser.open(playlist['external_urls']['spotify'])
-        print('Playlist has opened in your browser.')
-    elif choice == 0:
-        break
-    else:
-        print("Enter valid choice.")
+def openPlaylist(playlist_id):
+    playlist = spotifyObject.playlist(playlist_id)
+    webbrowser.open(playlist['external_urls']['spotify'])
+    print('Playlist has opened in your browser.')
+
